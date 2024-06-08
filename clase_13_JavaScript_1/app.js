1d2f8d57d5168cf82a7ee2287b360aba1a339748
AndresF1994 <andres0519pa@gmail.com>
2024-06-08 14:01:10 -0500

    clase_1_JavaScript

---
+++ clase_13_JavaScript_1/app.js
@@ -0,0 +1,28 @@
+// Esto es un comentario en linea 
+/*esto es un
+comentatio 
+aflkaflamfa
+en bloque*/
+console.log('Vinculando con un archivo externo')
+
+// Variables
+let num; // Esta es una variable indefinida 
+let nombre = 'Nahitan'; // esta es una variable definida 
+console.log('Nahitan')
+console.log('nombre')
+
+/*
+let apellido = prompt('Indique su apellido');
+document.write('Bienvenido ', nombre,' ',apellido)
+*/
+
+var numero_1=Number(prompt('Digite el primer numero')) // var tambien us una de las formas de declarar una variable
+var numero_2=Number(prompt('Digite el segundo numero')) // var se utiliza para variales globales
+
+console.log('La suma de los dos numero es: ',numero_1+numero_2)//console.log se implementa para realizar imperesiones dentro de la consola
+
+suma=numero_1+numero_2;
+resta=numero_1-numero_2;
+
+numero4=isFinite(567);
+console.log(numero4)
